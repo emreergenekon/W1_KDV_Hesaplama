a5a3e6eacd4b53de42c9a46ec54bbe3ae56d214e
Emre Ergenekon <165706438+emreergenekon@users.noreply.github.com>
2024-04-22 20:21:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+package Patika_Dev_Bootcamp;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double tutar, kdvOran = 0.18, kdvTutar, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz: ");
+        tutar = input.nextDouble();
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz Tutar: " + tutar);
+        System.out.println("Kdv Oranı: " + kdvOran);
+        System.out.println("KDV Tutarı: " + kdvTutar);
+        System.out.println("KDV'li Tutarı: " + kdvliTutar);
+    }
+}
